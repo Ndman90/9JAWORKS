1d26b377d5f1ed7364cef6dd0b970cc77117225a
Ndman90 <onwuamaegbund99@gmail.com>
2024-10-17 02:45:32 +0100

    Post

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import linkupRoutes from "./routes/linkup.route.js";
 
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 dotenv.config(); 
 
@@ -17,6 +18,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
+app.use(cors({ 
+    origin: "http://localhost:5173",
+    credentials: true,
+})
+);
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
