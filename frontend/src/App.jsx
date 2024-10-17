d253b4e0780c5073969e748a75214182a725fef0
Ndman90 <onwuamaegbund99@gmail.com>
2024-10-17 22:19:54 +0100

    Complete Sections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,31 @@ import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/auth/LoginPage";
 import SignUpPage from "./pages/auth/SignUpPage";
-//import toast, { Toaster } from "react-hot-toast";
-//import { useQuery } from "@tanstack/react-query";
-//import { axiosInstance } from "./lib/axios";
-//import NotificationsPage from "./pages/NotificationsPage";
-//import NetworkPage from "./pages/NetworkPage";
-//import PostPage from "./pages/PostPage";
-i//mport ProfilePage from "./pages/ProfilePage";
+import toast, { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios";
+import NotificationsPage from "./pages/NotificationsPage";
+import NetworkPage from "./pages/NetworkPage";
+import PostPage from "./pages/PostPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
-	//const { data: authUser, isLoading } = useQuery({
-	//	queryKey: ["authUser"],
-	//	queryFn: async () => {
-	//		try {
-	//			const res = await axiosInstance.get("/auth/me");
-	//			return res.data;
-	//		} catch (err) {
-	//			if (err.response && err.response.status === 401) {
-	//				return null;
-	//			}
-	//			toast.error(err.response.data.message || "Something went wrong");
-	//		}
-	//	},
-	//});
+	const { data: authUser, isLoading } = useQuery({
+		queryKey: ["authUser"],
+		queryFn: async () => {
+			try {
+				const res = await axiosInstance.get("/auth/me");
+				return res.data;
+			} catch (err) {
+				if (err.response && err.response.status === 401) {
+					return null;
+				}
+				toast.error(err.response.data.message || "Something went wrong");
+			}
+		},
+	});
 
-	//if (isLoading) return null;
+	if (isLoading) return null;
 
 	return (
 		<Layout>
